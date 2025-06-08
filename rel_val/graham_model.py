85a604aa2cf748556f167f322df68a656d1183ba
george123wright <george123wright@gmail.com>
2025-06-08 18:12:25 +0100

    Create graham_model.py

---
+++ rel_val/graham_model.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from pbv import bvps
+
+def graham_number(
+    pe_ind,
+    eps_low, eps_avg, eps_high,
+    price, pb_ind,
+    bvps_0, dps,
+    low_eps_y, avg_eps_y, high_eps_y
+):
+    eps_list  = pd.Series([eps_low, eps_avg, eps_high, low_eps_y, avg_eps_y, high_eps_y]).dropna()
+    pe_list = pd.Series(
+        [pe_ind['Region-Industry'], pe_ind['Industry-MC']]
+    ).dropna()
+    
+    ptb_list = pd.Series(
+        [pb_ind['Region-Industry'], pb_ind['Industry-MC']]
+    ).dropna()
+    
+    lb = 0.2 * price
+    ub = 5 * price
+    gn_vals = []
+    for e in eps_list:
+        fv = bvps(e, bvps_0, dps)
+        for p in pe_list:
+            for pb in ptb_list:
+                val = np.sqrt(p * float(e) * float(pb) * fv)
+                gn_vals.append(np.clip(val, lb, ub))
+
+    gn = np.array(pd.Series(gn_vals).dropna())
+    
+    if len(gn)!=0:
+        rel_ret = (gn / price) - 1
+
+        return (
+            np.min(gn),         
+            np.mean(gn),        
+            np.max(gn),            
+            np.mean(rel_ret),     
+            np.std(rel_ret)       
+        )
+    else:
+        return 0, 0, 0, 0, 0
