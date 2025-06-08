cb6391a1dec245b562d09f5f2fc90399f4d95c66
george123wright <george123wright@gmail.com>
2025-06-08 17:59:34 +0100

    Create sector_map.py

---
+++ maps/sector_map.py
@@ -0,0 +1,13 @@
+SectorMap = {
+    'basic-materials': 'Materials',
+    'communication-services' : 'Communication Services',
+    'consumer-cyclical': 'Consumer Discretionary',
+    'consumer-defensive': 'Consumer Staples',
+    'energy': 'Energy',
+    'financial-services': 'Financials',
+    'healthcare': 'Healthcare',
+    'industrials': 'Industrials',
+    'real-estate': 'Real Estate',
+    'technology': 'Technology',
+    'utilities': 'Utilities'
+}
