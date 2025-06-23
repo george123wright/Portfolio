aa7c134a206516967e8aa6f74c07a708a2dd3928
george123wright <george123wright@gmail.com>
2025-06-24 00:35:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,19 @@
 import datetime as dt
+from pathlib import Path
 
 TODAY = dt.date.today()
 YEAR_AGO = TODAY - dt.timedelta(days=365)
 FIVE_YEAR_AGO = TODAY - dt.timedelta(days=5 * 365)
 
-BASE_DIR = ""
+BASE_DIR = Path("/Users/georgewright")
 
 DATA_FILE = BASE_DIR / f"Portfolio_Optimisation_Data_{TODAY}.xlsx"
 FORECAST_FILE = BASE_DIR / f"Portfolio_Optimisation_Forecast_{TODAY}.xlsx"
-MODEL_FILE = BASE_DIR / f"Portfolio_Optimisation_DCF.xlsx"
+MODEL_FILE = BASE_DIR / "Portfolio_Optimisation_DCF.xlsx"
 
-ROOT_FIN_DIR = BASE_DIR / "modelling/stock_analysis_data"
+ROOT_FIN_DIR = BASE_DIR / "modelling" / "stock_analysis_data"
 
 RF = 0.046
-
-lbr = 0.2
-ubr = 5
-
+lbr, ubr = 0.2, 5
 MAX_WEIGHT = 0.1
-
 MONEY_IN_PORTFOLIO = 4000
