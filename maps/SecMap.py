eecb856faca061f3ebe606500081095be082a47d
george123wright <george123wright@gmail.com>
2025-07-27 22:30:02 +0100

    Create SecMap.py

---
+++ maps/SecMap.py
@@ -0,0 +1,13 @@
+sec_map = {
+    "^YH101": "Materials",
+    "^YH308": "Communication Services",
+    "^YH102": "Consumer Discretionary",
+    "^YH205": "Consumer Staples",
+    "^YH309": "Energy",
+    "^YH103": "Financials",
+    "^YH206": "Healthcare",
+    "^YH310": "Industrials",
+    "^YH104": "Real Estate",
+    "^YH311": "Technology",
+    "^YH207": "Utilities"
+}
