5c7f0559cfefc9208e4ce34e8dedf7c2b1ddcec0
george123wright <george123wright@gmail.com>
2025-08-11 00:02:45 +0100

    Create index_exchange_mapping.py

---
+++ maps/index_exchange_mapping.py
@@ -0,0 +1,14 @@
+ExchangeMapping = {
+    'NASDAQGS': '^NDX',  
+    'NASDAQGM': '^NDX',
+    'NASDAQCM': '^NDX',
+    'LSE': '^FTSE',   
+    'NYSE': '^GSPC',  
+    'XETRA': '^GDAXI',  
+    'MCE': '^IBEX',  
+    'AMSTERDAM':'^AEX',   
+    'PARIS': '^FCHI',   
+    'TORONTO': '^GSPTSE',
+    'HKSE': '^HSI',  
+    'SWISS': '^SSMI', 
+}
